3e609da8de3e732bbcdc4aa7d32edb20fae29668
Alex MacLure <almaclure@gmail.com>
2021-09-18 16:58:32 -0400

    Remove hardcoded urls from useApplicationData.js and use process.env variable instead

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -5,54 +5,61 @@ import axios from "axios";
 //Function (custom hook) that exports an array of states and functions required in Application.js (cleans up Application.js)
 export default function useApplicationData () {
 
+  // const webSocket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+
+  // webSocket.onopen = function (event) {
+  //       // webSocket.send("ping");
+  //       console.log("!!!!!Websocket opened!!!!!!")
+  //     };
+
   const [state, setState] = useState({
-    day: "Monday",
+    day: "",
     days: [],
     appointments: {
-      1: {
-        id: 1,
-        time: "12pm",
-      },
-      2: {
-        id: 2,
-        time: "1pm",
-        interview: {
-          student: "Lydia Miller-Jones",
-          interviewer: {
-            id: 1,
-            name: "Sylvia Palmer",
-            avatar: "https://i.imgur.com/LpaY82x.png",
-          }
-        }
-      },
-      3: {
-        id: 3,
-        time: "2pm",
-      },
-      4: {
-        id: 4,
-        time: "3pm",
-        interview: {
-          student: "Alex M",
-          interviewer: {
-            id: 5,
-            name: "Sven Jones",
-            avatar: "https://i.imgur.com/twYrpay.jpg",
-          }
-        }
-      },
-      5: {
-        id: 5,
-        time: "4pm",
-        interview: {
-          student: "John Cena",
-          interviewer: {
-            id: 3,
-            name: "Mildred Nazir",
-            avatar: "https://i.imgur.com/T2WwVfS.png",
-          }
-        }
-      }
+      // 1: {
+      //   id: 1,
+      //   time: "12pm",
+      // },
+      // 2: {
+      //   id: 2,
+      //   time: "1pm",
+      //   interview: {
+      //     student: "Lydia Miller-Jones",
+      //     interviewer: {
+      //       id: 1,
+      //       name: "Sylvia Palmer",
+      //       avatar: "https://i.imgur.com/LpaY82x.png",
+      //     }
+      //   }
+      // },
+      // 3: {
+      //   id: 3,
+      //   time: "2pm",
+      // },
+      // 4: {
+      //   id: 4,
+      //   time: "3pm",
+      //   interview: {
+      //     student: "Alex M",
+      //     interviewer: {
+      //       id: 5,
+      //       name: "Sven Jones",
+      //       avatar: "https://i.imgur.com/twYrpay.jpg",
+      //     }
+      //   }
+      // },
+      // 5: {
+      //   id: 5,
+      //   time: "4pm",
+      //   interview: {
+      //     student: "John Cena",
+      //     interviewer: {
+      //       id: 3,
+      //       name: "Mildred Nazir",
+      //       avatar: "https://i.imgur.com/T2WwVfS.png",
+      //     }
+      //   }
+      // }
     },
     interviewers: {}
   });
@@ -94,7 +101,9 @@ export default function useApplicationData () {
       return {...day, spots: remainingSpots(day, appointments)  }
     });
 
-    const queryURL = `http://localhost:8001/api/appointments/${id}`;
+    // const queryURL = `http://localhost:8001/api/appointments/${id}`;
+    const queryURL = `${process.env.REACT_APP_API_BASE_URL}/api/appointments/${id}`;
+    
     //Query server to add newly created interview object to appointments
     let bookReturn = axios.put(queryURL, {interview});
 
@@ -139,28 +148,44 @@ export default function useApplicationData () {
       return {...day, spots: remainingSpots(day, appointments)  }
     });
 
-    const queryURL = `http://localhost:8001/api/appointments/${id}`;
+
+    // const queryURL = `http://localhost:8001/api/appointments/${id}`;
+    const queryURL = `${process.env.REACT_APP_API_BASE_URL}/api/appointments/${id}`;
+
+
     //Query server to add newly created interview object (with null interview spot) to appointments
     let cancelReturn = axios.delete(queryURL);
 
     //Set state to include new appointments and daysNew
     cancelReturn.then(() => { setState((prev) => ({...prev, appointments, days: daysNew}))});
 
+    console.log("CANCEL STATE", state)
     return cancelReturn;
   };
 
+  
   //Perform 3 get requests at same time and wait for all to respond before setting state (data is dependant on each other)
+  //Uses axios base url defined in .env.development or .env.test and appens /api/days etc to it for each request
   useEffect(()=> {
     Promise.all([
-      axios.get('http://localhost:8001/api/days'),
-      axios.get('http://localhost:8001/api/appointments'),
-      axios.get('http://localhost:8001/api/interviewers')
+      axios.get(`/api/days`),
+      axios.get(`/api/appointments`),
+      axios.get(`/api/interviewers`)
     ]).then((all) => {
+
+      // console.log("ALLLLL", all);
+      // console.log("ALLLLL0data", all[0].data);
+      // console.log("ALLLLL1data", all[1].data);
+      // console.log("ALLLLL2data", all[2].data);
   
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
-   
-    });
+
+      console.log("axios3 State SYNC......", state);
+
+    })
   }, []);
+
+  
 
   // //Stretch work webs socket
 
@@ -168,12 +193,14 @@ export default function useApplicationData () {
     //Setup new websocket connection by creating webSocket object
     const webSocket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
 
-    //When new websocket connection opens, send "ping" to server
-    webSocket.onopen = function (event) {
-      webSocket.send("ping");
-    };
+    // //When new websocket connection opens, send "ping" to server
+    // webSocket.onopen = function (event) {
+    //   webSocket.send("ping");
+    // };
 
-    //When message recived from server
+    console.log("USE EFFECT WS Start State ", state);
+
+
     webSocket.onmessage = function (event) {
 
       //Convert returned string message to object via JSON.parse
@@ -181,11 +208,20 @@ export default function useApplicationData () {
       //Console log the message
       console.log("Message Recieved: ", msg);
 
-      // setState((prev) => {...prev, } )
+      console.log("msg.id", msg.id);
 
-    }
+      console.log("msg.interview", msg.interview);
+
+      console.log("msg recieved state", state);
+
+      setState((prev) => ({...prev}));
+    
+    // if (msg) {
+    //   setState(state.appointments[msg.id].interview = msg.interview)
+    // }
+    };
+
   }, []);
-
  
   
   
--- src/index.js
+++ src/index.js
@@ -5,4 +5,11 @@ import "index.scss";
 
 import Application from "components/Application";
 
+import axios from 'axios';
+
+//Set the base URL of axios to the process.env variable we put
+if (process.env.REACT_APP_API_BASE_URL) {  
+   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL; 
+  }
+
 ReactDOM.render(<Application />, document.getElementById("root"));
